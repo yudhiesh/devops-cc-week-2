689a8bb972a1f70fec5ae75458fa5839e73e05ed
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2023-10-31 21:11:51 +0800

    init base repo

---
+++ quote_disp/app.py
@@ -0,0 +1,28 @@
+import random
+
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def health():
+    return "healthy"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get_quote")
+def quote():
+    quote = requests.get("http://gen:5000/quote").text
+    print("quote - ", quote)
+
+    return render_template("quote.html", quote=quote)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5001)
---
+++ quote_gen/app.py
@@ -0,0 +1,43 @@
+import random
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+quotes = [
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+    "The way to get started is to quit talking and begin doing. -Walt Disney",
+    "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
+    "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+    "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+]
+
+
+@app.route("/health")
+def health():
+    return "healthy"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/quote")
+def quote():
+    quotes = [
+        "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+        "The way to get started is to quit talking and begin doing. -Walt Disney",
+        "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
+        "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+        "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+        "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+    ]
+
+    index = random.randrange(len(quotes))
+    return str(quotes[index])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
